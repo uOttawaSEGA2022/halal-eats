02e8ddf55b86de522cda18a02ed59371ccb0dc28
Saba Alowaini <salow068@uottawa.ca>
2022-10-19 19:18:58 -0400

    connected firebase to the app as you now can store the user data in the firebase database

--- app/src/main/java/com/example/mealerapp/MainActivity.java
+++ app/src/main/java/com/example/mealerapp/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //firebaseDatabase.getReference().child("admin").child("SophiaKey").setValue("1234566");
+        //firebaseDatabase.getReference().child("admin").child("ShooqKey").setValue("1234566");
+        //firebaseDatabase.getReference().child("admin").child("SophiaKey").setValue("0000000");
+
+        //firebaseDatabase.getReference().child("clinet").setValue("Saba");
+
     }
 }
