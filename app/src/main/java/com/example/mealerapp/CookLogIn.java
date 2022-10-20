ba08c47982a8460c298fbd01fab5445b0fe61a1d
Fbaylri <fatmahbayrli@gmail.com>
2022-10-20 17:47:45 -0400

    added mock layout for cook registration

---
+++ app/src/main/java/com/example/mealerapp/CookLogIn.java
@@ -0,0 +1,14 @@
+package com.example.mealerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CookLogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cook_log_in);
+    }
+}
