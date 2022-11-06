c3ba69f7b3cc51ff2f7c64f07eddac382396e7e8
Saba Alowaini <salow068@uottawa.ca>
2022-11-06 16:29:51 -0500

    added the list of complaints

--- app/src/main/java/com/example/mealerapp/AdminViewComplaintsPage.java
+++ app/src/main/java/com/example/mealerapp/AdminViewComplaintsPage.java
@@ -1,14 +1,83 @@
 package com.example.mealerapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import android.widget.Spinner;
+import android.widget.AdapterView;
+import android.view.View;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.os.Bundle;
+
+import java.util.List;
+
 public class AdminViewComplaintsPage extends AppCompatActivity {
+
+
+    Spinner spinner;
+    DatabaseReference databaseReference;
+    ArrayList<String> complaints = new ArrayList<>();
+    //List<String> names;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_view_complaints_page);
+
+
+
+        spinner = (Spinner) findViewById (R.id.spinner);
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference.child("complaints").addValueEventListener(new ValueEventListener() {
+
+            public void onDataChange (@NonNull DataSnapshot dataSnapshot){
+                for (DataSnapshot item : dataSnapshot.getChildren()) {
+                    //String spinnerName = chilSnapshot.child("username").getValue(String.class);
+                    //names.add(spinnerName);
+
+                   // String spinnerComplaint = item.child("complaint").getValue(String.class);
+                     String spinnerComplaint = item.getValue().toString();
+
+                    complaints.add(spinnerComplaint);
+
+
+                }
+
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(AdminViewComplaintsPage.this, android.R.layout.simple_spinner_dropdown_item, complaints);
+                arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(arrayAdapter);
+
+            }
+
+
+            @Override
+            public void onCancelled (@NonNull DatabaseError error){
+
+            }
+
+        });
     }
 }
