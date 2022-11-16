ab4f607a4386cffee76073c3d4ff3642680d8cd8
sohpha <slube042@uottawa.ca>
2022-11-16 13:07:27 -0500

    - added log off feature to cook suspension notification pages

--- app/src/main/java/com/example/mealerapp/PermanentlySuspended.java
+++ app/src/main/java/com/example/mealerapp/PermanentlySuspended.java
@@ -2,13 +2,29 @@ package com.example.mealerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PermanentlySuspended extends AppCompatActivity {
+
+    private Button log_off;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permanently_suspended);
+
+        log_off = (Button) findViewById(R.id.log_off);
+        log_off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { openHomePage();  }
+        });
+    }
+
+    public void openHomePage(){
+        Intent intent = new Intent(this, InitialWelcome.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/mealerapp/TemporarilySuspended.java
+++ app/src/main/java/com/example/mealerapp/TemporarilySuspended.java
@@ -5,20 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class TemporarilySuspended extends AppCompatActivity {
+    private Button log_off;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temporarily_suspended);
         TextView dateDisplay = findViewById(R.id.date);
         Intent intent = getIntent();
         String susLiftedDate = intent.getStringExtra("Date");
         dateDisplay.setText(susLiftedDate);
+        log_off = (Button) findViewById(R.id.log_off2);
+        log_off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { openHomePage();  }
+        });
 
+    }
+
+    public void openHomePage(){
+        Intent intent = new Intent(this, InitialWelcome.class);
+        startActivity(intent);
     }
 
 }
