05f781d93d88e84de7f140acd7a20361d6a141f7
Abdullah <abdullahrama@hotmail.com>
2022-12-03 05:44:58 -0500

    added xml views

--- app/src/main/java/com/example/mealerapp/CookViewProfile.java
+++ app/src/main/java/com/example/mealerapp/CookViewProfile.java
@@ -1,5 +1,6 @@
 package com.example.mealerapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,7 +8,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 public class CookViewProfile extends AppCompatActivity {
+
+    DatabaseReference ref;
 
     private Button returnHome;
 
